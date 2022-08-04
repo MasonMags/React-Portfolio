c763f9d44f095c101753b36d197debf109580eb6
Mason Maglalang <MasonMaglalang1@gmail.com>
2022-08-04 12:29:32 -0500

    rendering about me section

---
+++ src/components/pages/About.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function About() {
+    return (
+        <div class="row">
+            <h1 class="column title" id="about-me"> About Me </h1>
+                <div class="column content-boxes">
+                 <img class="flex-child" src="" alt="Mason on the river" id="about-me-img"></img>
+                    <p id="about-blurb">
+                         Hello! My name is Mason Maglalang and I'm a full-stack web developer based out of Austin, Texas. 
+                        I'm a creative thinker and a problem solver who loves to design, build, and implement things that 
+                        live on the internet. In my free time, I'm a competitive weightlifter and nature enjoyer. &#128526
+                    </p>
+                </div>
+        </div>
+    );
+}
