84ad37f07449ae264166bdb67effca3a9d3212f8
Mason Maglalang <MasonMaglalang1@gmail.com>
2022-08-04 14:17:48 -0500

    added skills page

---
+++ src/components/pages/Skills.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+function Skills() {
+    return (
+        <div class="row">
+  <h1 class="column title" id="skills">Skills</h1>
+  <div class="column content-boxes">
+      <ul class="column content-boxes" id="skills-list">
+        <li>HTML</li>
+        <li>CSS</li>
+        <li>JavaScript</li>
+        <li>React.js</li>
+        <li>Node.js</li>
+        <li>Express.js</li>
+        <li>Handlebars.js</li>
+        <li>Sequelize.js</li>
+        <li>MySQL</li>
+        <li>NoSQL</li>
+        <li>MongoDB</li>
+        <li>Mongoose ODM</li>
+      </ul>
+  </div>
+</div>
+    );
+};
+
+export default Skills;
