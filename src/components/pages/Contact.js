31720504b2debe35a6bdb0bbefb2d27dcbde28eb
Mason Maglalang <MasonMaglalang1@gmail.com>
2022-08-06 13:13:48 -0500

    contact form jsx

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -4,21 +4,20 @@ import React from 'react';
 
 export default function Contact() {
     return(
-        <div>
-            {/* <h1 className="title">Contact Me</h1> */}
-            <ul class="column content-boxes contact-links">
-                <li class="cm-links">
-                    <a href="mailto:MasonMaglalang1@gmail.com"  id="email-link">Email</a>
-                </li>
-
-                <li class="cm-links">
-                    <a href="https://github.com/MasonMags" >Github</a>
-                </li>
-
-                <li class="cm-links">
-                     <a href="https://www.linkedin.com/in/mason-maglalang-930ba322a/">LinkedIn</a>
-                </li>
-            </ul>
-        </div>
+        <form class="contact" id="contact-form">
+    <div className="form-group">
+        <label htmlFor="name">Name</label>
+        <input type="text" className="form-control" />
+    </div>
+    <div className="form-group">
+        <label htmlFor="exampleInputEmail1">Email address</label>
+        <input type="email" className="form-control" aria-describedby="emailHelp" />
+    </div>
+    <div className="form-group">
+        <label htmlFor="message">Message</label>
+        <textarea className="form-control" rows="5"></textarea>
+    </div>
+    <button type="submit" className="btn btn-primary btn-overwrite">Submit</button>
+</form>
     );
 };
