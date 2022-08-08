e61aa7ed2607159cfdbf11b3fe900428c6feb252
Mason Maglalang <MasonMaglalang1@gmail.com>
2022-08-08 16:45:31 -0500

    generating downloadable resume pdf

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -1,11 +1,11 @@
 import React from 'react';
+import resume from '../../resume/resume.pdf'
 
 export default function Resume() {
     return (
         <div>
-            {/* <h1>Resume</h1> */}
-            <p>
-                Render here using React PDF package
+            <p class="resume-content">
+               Take a look at my resume <a href={resume} target='_blank' rel="noreferrer" class="resume-overwrite">here</a>!
             </p>
         </div>
     );
