9b2eb42b8cd282a3278d903c6a3337f79d6f2c4f
Mason Maglalang <MasonMaglalang1@gmail.com>
2022-08-08 19:58:17 -0500

    added skills list

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -7,6 +7,23 @@ export default function Resume() {
             <p class="resume-content">
                Take a look at my resume <a href={resume} target='_blank' rel="noreferrer" class="resume-overwrite">here</a>!
             </p>
+            <p class="tech">Additionally, here is a list of my technical proficiencies:</p>
+            <div class="content-box">
+                <ul class="skills-list">
+                    <li>HTML</li>
+                    <li>CSS</li>
+                    <li>JavaScript</li>
+                    <li>React.js</li>
+                    <li>Node.js</li>
+                    <li>Express.js</li>
+                    <li>Handlebars.js</li>
+                    <li>Sequelize.js</li>
+                    <li>MySQL</li>
+                    <li>NoSQL</li>
+                    <li>MongoDB</li>
+                    <li>Mongoose ODM</li>
+                </ul>
+             </div>
         </div>
     );
 };
